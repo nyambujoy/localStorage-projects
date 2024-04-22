36c2c8493f0d59b2518ac122d389cf399ccf7ad0
nyambujoy <joyliznyambura@gmail.com>
2024-04-22 16:35:23 +0300

    corrected game logic

--- numGuess/app.js
+++ numGuess/app.js
@@ -3,52 +3,43 @@ const submit = document.getElementById('submit')
 const playAgain = document.getElementById('again')
 const resultP = document.querySelector('.bottom')
 const correct = document.querySelector('.correct')
-const newBtn = document.querySelector('#new')
+// const newBtn = document.querySelector('#new')
 console.log(userInput)
 let running = true
 let attempts = 0
 
+
 let ans = Math.floor(Math.random() * 101)
 
 const handleSubmit = () => {
+    let userValue = Number(userInput.value.trim());
 
-    while (running) {
-        let userValue = Number(userInput.value.trim())
-        // console.log(typeof (userValue))
-
-        console.log(userValue, ans)
-        if (isNaN(userValue)) {
-            correct.textContent = 'please enter a number'
-        } else if (userValue > 100 || userValue < 0) {
-            correct.textContent = 'please enter a number between 0 & 100'
-        } else {
-            attempts++
-            if (userValue > ans) {
-                resultP.textContent = 'guess lower'
-            } else if (userValue < ans) {
-                resultP.textContent = 'guess higher'
-
-            } else if (userValue === ans) {
-                resultP.textContent = `You go it right!! in ${attempts} `
-                correct.textContent = ans
-                running = false
-            }
-
-
-
-
-
+    if (isNaN(userValue)) {
+        correct.textContent = 'Please enter a number.';
+    } else if (userValue > 100 || userValue < 0) {
+        correct.textContent = 'Please enter a number between 0 & 100.';
+    } else {
+        attempts++;
+        if (userValue > ans) {
+            resultP.textContent = 'Guess lower.';
+        } else if (userValue < ans) {
+            resultP.textContent = 'Guess higher.';
+        } else if (userValue === ans) {
+            resultP.textContent = `You got it right!! It took you ${attempts} attempts.`;
+            correct.textContent = ans;
+            running = false;
+            // Disable the input and submit button
+            userInput.disabled = true;
+            submit.disabled = true;
         }
-
-
-
     }
+};
 
 
 
 
 
-}
+
 
 const showAnswer = () => {
 
@@ -57,18 +48,14 @@ const showAnswer = () => {
 
 }
 
-function handleRound() {
-    let ans = Math.floor(Math.random() * 101)
-    handleSubmit()
-    showAnswer()
-}
+
 
 
 
 
 submit.addEventListener("click", handleSubmit)
 playAgain.addEventListener("click", showAnswer)
-newBtn.addEventListener("click", handleRound)
+
 
 
 
